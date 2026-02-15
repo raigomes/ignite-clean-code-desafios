cd553ad2bcba8ff800dd48cb160024f1dfc102d1
raigomes <raigomes2@hotmail.com>
2026-02-15 17:25:06 -0300

    Change state name

--- desafios/03-causa-vs-efeito.tsx
+++ desafios/03-causa-vs-efeito.tsx
@@ -18,24 +18,24 @@ function fetchUser() {
 }
 
 export function UserProfile() {
-  const [shouldNotRenderUserName, setShouldNotRenderUserName] = useState(false)
+  const [isGettingUserDataApi, setIsGettingUserDataApi] = useState(false)
   const [userData, setUserData] = useState<User>()
 
   useEffect(() => {
     function loadUser() {
-      setShouldNotRenderUserName(true)
+      setIsGettingUserDataApi(true)
 
       const fetchUserResponse = fetchUser()
 
       setUserData(fetchUserResponse.data.user)
       
-      setShouldNotRenderUserName(false)
+      setIsGettingUserDataApi(false)
     }
 
     loadUser()
   })
 
-  if (shouldNotRenderUserName) {
+  if (isGettingUserDataApi) {
     return <p>Loading...</p>
   }
 
