6a96ade02de8ea6e2117978f90ccee62dd80ea0f
raigomes <raigomes2@hotmail.com>
2026-02-15 18:22:42 -0300

    Refactor conditional

--- desafios/05-regras-em-condicionais.ts
+++ desafios/05-regras-em-condicionais.ts
@@ -1,19 +1,26 @@
-const necessaryGradeToBeApproved = 7
-const studentGrade = 10
+const necessaryGradeToBeApproved = 7;
+const studentGrade = 10;
 
-const numberOfAbsensesToFailSchool = 100
-const studentNumberOfAbsenses = 109
+const numberOfAbsensesToFailSchool = 100;
+const studentNumberOfAbsenses = 109;
 
 function checkIfStudentPassedTheSchoolYear() {
   if (studentGrade < necessaryGradeToBeApproved) {
-    return { error: true, message: 'Student was not approved because his grade was below the necessary.'}
-  } else {
-    if(studentNumberOfAbsenses > numberOfAbsensesToFailSchool) {
-      return { error: true, message: 'Student was not approved because of his absenses'}
-    } else {
-      return { error: false, message: 'Student was approved :)'}
-    }
+    return {
+      error: true,
+      message:
+        "Student was not approved because his grade was below the necessary.",
+    };
   }
+
+  if (studentNumberOfAbsenses > numberOfAbsensesToFailSchool) {
+    return {
+      error: true,
+      message: "Student was not approved because of his absenses",
+    };
+  }
+
+  return { error: false, message: "Student was approved :)" };
 }
 
-console.log(checkIfStudentPassedTheSchoolYear())
+console.log(checkIfStudentPassedTheSchoolYear());
