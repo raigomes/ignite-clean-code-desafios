38f21779463e5d071e8aefba8ed2199b8d86ff50
raigomes <raigomes2@hotmail.com>
2026-02-15 21:26:44 -0300

    Parameter destructuring

--- desafios/06-parametros-e-desestruturacao.tsx
+++ desafios/06-parametros-e-desestruturacao.tsx
@@ -1,11 +1,67 @@
-function updateUserRoute(body, params) {
-  updateUserController(body, params)
+//Descrição: https://efficient-sloth-d85.notion.site/Desafio-Par-metros-e-Desestrutura-o-e7b4ac605bf9467da7b032573bc8adb9
+
+function updateUserRoute({
+  body = { name: "", email: "", password: "" },
+  params = { id: -1 },
+}) {
+  const { name, email, password } = body;
+  const { id } = params;
+
+  updateUserController({
+    data: {
+      name,
+      email,
+      password,
+    },
+    params: {
+      id,
+    },
+  });
 }
 
-function updateUserController(data, params) {
-  userRepository.update(data, params)
+function updateUserController({ data, params }) {
+  const { name, email, password } = data;
+  const { id } = params;
+
+  userRepository.update({
+    data: {
+      name,
+      email,
+      password,
+    },
+    params: {
+      id,
+    },
+  });
 }
 
 const userRepository = {
-  update: (data, params) => {},
-}
+  update: ({ data, params }) => {
+    console.log(data, params);
+  },
+};
+
+updateUserRoute({
+  body: {
+    name: "Raí",
+    email: "mail@mail.com",
+    password: "admin",
+  },
+  params: {
+    id: 1,
+  },
+});
+
+updateUserRoute({
+  params: {
+    id: 1,
+  },
+});
+
+updateUserRoute({
+  body: {
+    name: "Raí",
+    email: "mail@mail.com",
+    password: "admin",
+  },
+});
