ceef62ce35ca2d61d4aefb4af20f519f20fd11df
raigomes <raigomes2@hotmail.com>
2026-02-16 01:09:31 -0300

    Remove "magic numbers" and rename function parameters

--- desafios/07-numeros-magicos.js
+++ desafios/07-numeros-magicos.js
@@ -1,5 +1,7 @@
+// Descrição: https://efficient-sloth-d85.notion.site/Desafio-N-meros-M-gicos-ca206dedc5e3436cb09b86658002de00
+
 function lookForUpdades() {}
 
-setInterval(lookForUpdades, 60 * 30 * 1000)
+setInterval(lookForUpdades, 1000 * 60 * 30); // 30 minutes
 
-function calculateDiscount(price, discount) {}
+function calculateDiscount(priceInCents, discountInPercentage) {}
