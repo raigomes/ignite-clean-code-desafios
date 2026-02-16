c395dc8d8ca128fcdd11975947965ec8fe45d8a5
raigomes <raigomes2@hotmail.com>
2026-02-16 01:25:19 -0300

    Remove syntax sugar

--- desafios/09-syntatic-sugars.ts
+++ desafios/09-syntatic-sugars.ts
@@ -1,26 +1,27 @@
+// Descrição: https://efficient-sloth-d85.notion.site/Desafio-Syntatic-Sugars-7400d576da8e4d98968e1523ad7efcef
 function getFirstFiveRatings(ratings) {
-  return ratings.length >= 5 && ratings.slice(0, 5)
+  return ratings.length >= 5 && ratings.slice(0, 5);
 }
 
 function sumFirstFiveRatings(ratings) {
-  const ratingsBool = !!ratings
+  const ratingsBool = Boolean(ratings);
 
   if (ratingsBool) {
-    const firstFiveRatings = getFirstFiveRatings(ratings)
+    const firstFiveRatings = getFirstFiveRatings(ratings);
 
-    if (!firstFiveRatings) return { error: 'there must be at least 5 ratings' }
+    if (!firstFiveRatings) return { error: "there must be at least 5 ratings" };
 
     let ratingsSum = 0;
 
     for (const rating of firstFiveRatings) {
-      ratingsSum += +rating
+      ratingsSum += Number(rating);
     }
 
-    return { ratingsSum, created_at: +new Date() }
+    return { ratingsSum, created_at: Number(new Date()) };
   }
 
-  return { error: 'ratings is required' }
+  return { error: "ratings is required" };
 }
 
-const appRatings = ['5', '3', '4', '4', '5', '1', '5', '4', '4', '3']
-sumFirstFiveRatings(appRatings)
+const appRatings = ["5", "3", "4", "4", "5", "1", "5", "4", "4", "3"];
+console.log(sumFirstFiveRatings(appRatings));
